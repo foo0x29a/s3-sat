1d53108242b85f74950b6dc322f1683d14f61916
foo0x29a <thiago.nobayashi@gmail.com>
2020-07-23 06:56:12 +0900

    Add basic arg parser

---
+++ s3_poker.py
@@ -0,0 +1,15 @@
+import argparse
+from src.main import start_workers
+
+def workers_type(n):
+    n = int(n)
+    if n <= 0:
+        raise argparse.ArgumentTypeError("Minimum workers is 1")
+    return n
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--workers","-w", type=workers_type, help="Number of workers. A good heuristic is: max((number_of_cpus - 1), 1)", default = 1)
+    args = parser.parse_args()
+    start_workers(args.workers)
